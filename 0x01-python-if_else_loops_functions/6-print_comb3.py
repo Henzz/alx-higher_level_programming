d55034f3eb31c68a409cc7d585adcab5c1d56054
Henzz <khenok53@gmail.com>
2022-12-05 06:10:57 -0800

    task 6 added format()

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i != j and i < j:
             if i == 8 and j == 9:
-                print(f'{i}{j}')
+                print(f'{i}{j}'.format(i, j))
             else:
-                print(f'{i}{j}', end=', ')
+                print(f'{i}{j}'.format(i, j), end=', ')
