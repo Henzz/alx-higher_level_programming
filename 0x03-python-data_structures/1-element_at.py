b451688ec73e27bc8b9aa8a28009c9a6a9107edd
Henzz <khenok53@gmail.com>
2022-12-06 15:44:57 -0800

    task 1 added ValueError

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list[idx] == None and my_list[idx] < 0:
+    if my_list[idx] == ValueError or my_list[idx] < 0:
         return None
     if my_list[idx] >= 0:
         return my_list[idx]
