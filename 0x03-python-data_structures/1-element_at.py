3164dd35b033c7e3c8443a830ef0222612350d8c
Henzz <khenok53@gmail.com>
2022-12-06 15:56:55 -0800

    task 1 added len(my_list)

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list[idx] == ValueError or my_list[idx] < 0:
-        return None
-    if my_list[idx] >= 0:
+    if len(my_list) > abs(idx):
         return my_list[idx]
+    else:
+        return None
