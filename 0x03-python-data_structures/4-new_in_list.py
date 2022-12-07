a7f7a101adb5263ed089a11acaf686f3795e7670
Henzz <khenok53@gmail.com>
2022-12-07 09:38:34 +0300

    task 4 added condition to out of range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,3 +5,5 @@ def new_in_list(my_list, idx, element):
             new_list = list(my_list)
             new_list[idx] = element
             return new_list
+        else:
+            return my_list
