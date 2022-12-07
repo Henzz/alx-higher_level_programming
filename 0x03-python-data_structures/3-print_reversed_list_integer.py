0d6fbc7f095de604d3e7635599f2e980227ddeb5
Henzz <khenok53@gmail.com>
2022-12-06 16:29:14 -0800

    task 0 added :d and task 3 added condition to none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    k = -1
-    for i in my_list:
-        print("{:d}".format(my_list[k]))
-        k = k - 1
+    if my_list:
+        k = -1
+        for i in my_list:
+            print("{:d}".format(my_list[k]))
+            k = k - 1
