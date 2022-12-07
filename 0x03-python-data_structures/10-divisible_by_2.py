29e21dd85ef910568573a3fac14f94fb5ddf47fc
Henzz <khenok53@gmail.com>
2022-12-07 12:00:38 +0300

    task 10 modified for loop with length of list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     if len(my_list) != 0:
         new_list = list(my_list)
-        for i in my_list:
+        for i in range(len(my_list)):
             if my_list[i] % 2 == 0:
                 new_list[i] = True
             else:
