f62bb0ba863e35e97bd56bf5fdc3270375554e0e
Henzz <khenok53@gmail.com>
2022-12-10 01:07:02 +0300

    task 6 modified and task 7 modified

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update( {key : value} )
-    #for i in a_dictionary:
-        #print(f"{i} : {a_dictionary[i]}")
+    a_dictionary[key] = value
     return a_dictionary
