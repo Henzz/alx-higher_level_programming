21920bc922d675583eab98a9c286f0bd05e2eba0
Henzz <khenok53@gmail.com>
2022-12-09 10:54:01 +0300

    task 11 modified for idx if negative

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if len(my_list) != 0 and len(my_list) - 1 >= idx:
-        #new_list = my_list[0:idx] + my_list[idx+1:]
+    if idx >= 0 and len(my_list) != 0 and len(my_list) - 1 >= idx:
         my_list.remove(my_list[idx])
         return my_list
     else:
