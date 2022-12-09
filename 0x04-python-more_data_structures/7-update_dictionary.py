9c910bf527a652c9aa95652e34b207d1b5893d5c
Henzz <khenok53@gmail.com>
2022-12-10 00:49:45 +0300

    task 6 and task 7 done and first time commit

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary.update( {key : value} )
+    #for i in a_dictionary:
+        #print(f"{i} : {a_dictionary[i]}")
+    return a_dictionary
