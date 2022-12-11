ea406a208ba2ca25237b8a6b0cfb0712f69df4ab
Henzz <khenok53@gmail.com>
2022-12-11 14:32:44 +0300

    task 13 and header lists done and first commit..

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) > 96 and ord(c) < 123:
-            print(f"{chr(ord(c) - 32)}".format(c), end='\0')
-        else:
-            print(f"{c}".format(c), end='\0')
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
