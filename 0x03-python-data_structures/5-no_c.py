d8be7d4cd6611aa90a47eb2ec1f8970290053d18
Henzz <khenok53@gmail.com>
2023-03-01 14:49:09 +0300

    clearing tasks for sprint 2

--- 0x03-python-data_structures/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    if my_string:
-        new_string = list(my_string)
-        for i in range(0, len(new_string)):
-            if ord(new_string[i]) == 67 or ord(new_string[i]) == 99:
-                new_string[i] = ''
-        my_string = ''.join(new_string)
-    return my_string
