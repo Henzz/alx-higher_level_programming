5c075f54b0d2c0614e1c3e2ae9d7c881d5202d62
Henzz <khenok53@gmail.com>
2023-03-30 05:50:13 +0300

    project 5 exception done and project 6 done except advanced

---
+++ 0x06-python-classes/oldm/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Python program to demonstrate
+# empty class
+
+class Square:
+    """A simple empty class"""
+
+    def __init__(self):
+        # An empty block
+        pass  # An empty block
