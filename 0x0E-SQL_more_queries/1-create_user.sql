c940819ad5830cb4f6afe1e2bf130625d1bc8736
Henzz <khenok53@gmail.com>
2024-02-15 06:05:30 +0300

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,16 +5,15 @@ SELECT COUNT(*) INTO @userExists FROM mysql.user WHERE user = 'user_0d_1';
 
 -- If the user already exists, exit the script
 IF @userExists > 0 THEN
-    SELECT 'user_0d_1 already exists. No changes made.';
-    LEAVE;
+	SELECT 'user_0d_1 already exists. No changes made.';
+ELSE
+	-- Create user_0d_1 and grant all privileges
+	CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+	GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+	-- Flush privileges to apply changes
+	FLUSH PRIVILEGES;
+
+	-- Display a success message
+	SELECT 'user_0d_1 created with all privileges.';
 END IF;
-
--- Create user_0d_1 and grant all privileges
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
--- Display a success message
-SELECT 'user_0d_1 created with all privileges.';
