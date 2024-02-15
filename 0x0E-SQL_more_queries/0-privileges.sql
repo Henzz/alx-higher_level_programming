0aa93a84b938d0d378423b8acf1915b7f572f48e
Henzz <khenok53@gmail.com>
2024-02-15 05:31:41 +0300

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,7 @@
+-- List all privileges of the MYSQL users of 'user_0d_1'
+-- and 'user_0d_2'
 USER1='user_od_1'
 USER2='user_od_2'
-SELECT * FROM mysql.user WHERE user IN ('$USER1', '$USER2');
+SELECT *
+FROM mysql.user
+WHERE user IN ('$USER1', '$USER2');
