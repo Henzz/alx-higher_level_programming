1bffcbaa5b4fbb4e4c9101be7ec3dda03e878045
Henzz <khenok53@gmail.com>
2024-02-15 05:51:42 +0300

    fix

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,10 +1,8 @@
 -- List all privileges of the MYSQL users of 'user_0d_1'
 -- and 'user_0d_2'
-USER1='user_od_1'
-USER2='user_od_2'
 
--- Query to retrieve privileges for USER1
-SHOW GRANTS FOR '$USER1'@'localhost';
+-- Query to retrieve privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- Query to retrieve privileges for USER2
-SHOW GRANTS FOR '$USER2'@'localhost';
+-- Query to retrieve privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
