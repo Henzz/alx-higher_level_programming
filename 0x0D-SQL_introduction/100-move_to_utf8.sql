1347b250405497844cf948bdb63b2a8ed6fa3365
Henzz <khenok53@gmail.com>
2024-02-17 00:27:19 +0300

    task 100 - updated

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,10 @@
 -- Converts 'hbtn_0c_0' DB to UTF8(utf8mb4,
 -- collate utf8mb4_unicode_ci) in your MySQL server
+
+-- Seleting the DB
+USE hbtn_0c_0;
+
+-- Converting
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mbr COLLATE utf8mb4_unicode_ci;
