d27abea923711bbf476ad0c0f2fec304c6f91322
Henzz <khenok53@gmail.com>
2024-02-17 02:23:57 +0300

    task 2 done - create database and add user with select privilege only

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- Creates the database 'hbtn_0d_2' and the user 'user_0d_2'
+
+-- Create database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Grant permission
+GRANT ALL ON SELECT TO 'user_0d_2'@'localhost';
