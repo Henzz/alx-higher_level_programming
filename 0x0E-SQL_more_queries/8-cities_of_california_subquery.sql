7aa41c2dc3c70f4c8038cae28d65ed3efac8dda4
Henzz <khenok53@gmail.com>
2024-02-17 03:52:12 +0300

    task 8 - fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,5 +3,5 @@
 -- Select cities
 SELECT cities.id, cities.name FROM cities, states
 WHERE cities.state_id = states.id
-	AND state.name = 'California'
+	AND states.name = 'California'
 ORDER BY cities.id ASC;
