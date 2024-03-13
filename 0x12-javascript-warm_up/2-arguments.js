af5f8549a7758df193784870469d3c6a19b19a20
Henzz <khenok53@gmail.com>
2024-03-13 09:10:57 +0300

    task 2 fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,8 +2,10 @@
 const { argv } = require('node:process');
 
 // Check length of arguments
-if (argv.length < 2) {
+if (argv.length < 3) {
   console.log('No argument');
+} else if (argv.length < 4) {
+  console.log('Argument found');
 } else {
   console.log('Arguments found');
 }
