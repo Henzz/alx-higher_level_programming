7a326863ec4e7637a2372629ce2c341dca9f51ee
Henzz <khenok53@gmail.com>
2024-03-14 06:00:17 +0300

    doing task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    const row = c.repeat(this.size);
+    for (let i = 0; i < this.size; i++) {
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
