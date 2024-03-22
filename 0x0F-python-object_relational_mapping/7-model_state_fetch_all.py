e37ae35208f5057fa22c6c99b1290f66c840ca40
Henzz <khenok53@gmail.com>
2024-03-23 02:46:11 +0300

    task 7 done

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''
+    A script that lists all states from a database provided
+    with search param
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, pwd, database):
+    '''
+        List all State object from the specified database
+    '''
+    # Create the engine to connect to the MySQL server
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}\
+            @localhost:3306/{database}')
+
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    session = sessionmaker(bind=engine)
+    session = session()
+
+    # Query the State objects from the database and sort by id
+    states = session.query(State).order_by(State.id).all()
+
+    # Display the results
+    for row in states:
+        print(f"{row.id}: {row.name}")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == '__main__':
+    # Check if all arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: python script.py \
+                <username> <password> <database> <state>")
+        sys.exit(1)
+
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
