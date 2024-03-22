aa43fa0547653da16c439e739cdbffbe49ec5506
Henzz <khenok53@gmail.com>
2024-03-23 00:30:52 +0300

    task 2 done

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''
+    A script that lists all states from the database hbtn_0e_0usa
+'''
+import MySQLdb
+import sys
+
+
+def list_states(username, pwd, database, search):
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=pwd,
+            db=database)
+
+    # Create a cursor object to execute SQL queries
+    cur = db.cursor()
+
+    # Execute the SQL query to retrieve states
+    cur.execute("SELECT * FROM states WHERE name LIKE '%"
+                + search + "%' ORDER BY id ASC")
+
+    # Fetch all the rows returned by the query
+    rows = cur.fetchall()
+
+    # Display the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    # Check if all arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: python script.py \
+                <username> <password> <database> <search>")
+        sys.exit(1)
+
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+
+    list_states(username, password, database, search)
