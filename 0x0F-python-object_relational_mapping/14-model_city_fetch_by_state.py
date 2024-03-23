b532cfd7d052c9e827b5b0a9ce3391962ee6d5ca
Henzz <khenok53@gmail.com>
2024-03-23 04:12:23 +0300

    task 14 doing

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+'''
+    A script that list's a passed param state id
+    from a database provided with search param
+'''
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import Base, City
+from model_state import State
+
+
+def list_states(username, pwd, database):
+    '''
+        List all State object from the specified database
+    '''
+    # Create the engine to connect to the MySQL server
+    engine = create_engine(f'mysql+mysqldb://{username}:{password}\
+            @localhost:3306/{database}')
+
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query all city objects and join with state object
+    citiess = session.query(City, State)\
+        .filter(City.state_id == State.id).order_by(City.id).all()
+
+    # Print values
+    for city, state in cities:
+        print(f"{state.name}: ({city.id}) {city.name}")
+
+    # Close the session
+    session.close()
+
+
+if __name__ == '__main__':
+    # Check if all arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: python 14-model_city_fetch_by_state.py \
+                <username> <password> <database>")
+        sys.exit(1)
+
+    # Retrieve command-line arguments
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    list_states(username, password, database)
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+A script tha contains class definition of a state
+'''
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    '''
+    A class defining table and its attributes, a city
+    '''
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullables=False)
