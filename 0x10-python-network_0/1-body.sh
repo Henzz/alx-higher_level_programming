9ddd170c7cfc28c5b50fee36b4abbf2bab616590
Henzz <khenok53@gmail.com>
2024-04-02 20:05:22 +0300

    curl task 1 fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Takes in a URL and sends a GET request the displays the body of the response
-curl -X GET -s -o /dev/null -w "%{http_code}\n" "$1" | grep -q 200 && curl -s "$1"
+curl -sb -X GET "$1"
+#-o /dev/null -w "%{http_code}\n"
+#"$1"
+#| grep -q 200 && curl -s "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and sends a request to it to get a message in the body response
-curl -sX GET "$1" -d "You got me!"
+curl -sX GET "$1" | exec 1>&2
