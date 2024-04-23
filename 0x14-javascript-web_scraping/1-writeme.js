9c02cb1ee9b0a464f7dedba8549ab43aec0aacab
Henzz <khenok53@gmail.com>
2024-04-23 21:37:01 +0300

    task 1 done js web scrap

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require('fs');
+const { argv } = require('node:process');
+
+let file = '';
+let str = '';
+
+argv.forEach(function (val, index) {
+  if (index === 2) {
+    file = val;
+  }
+  if (index === 3) {
+    str = val;
+  }
+});
+
+fs.writeFile(file, str, 'utf-8', function (err, data) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Written!');
+});
